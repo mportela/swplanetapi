0c87e5b7280bbffccd6f0bce0a2e3059e6ee009d
mportela <marcel.portela@gmail.com>
2018-04-09 19:30:53 -0300

    + pep8 fix in app.py

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ app.config['MONGO_DBNAME'] = 'swapidb'
 app.config['MONGO_URI'] = 'mongodb://mongodb:27017/{}'.format(
     app.config['MONGO_DBNAME'])
 
+
 @app.route('/planet', methods=['GET'])
 def get_all_planets():
     planet = mongo.db.planets
@@ -20,35 +21,38 @@ def get_all_planets():
         output.append({'_id': str(p['_id']), 'nome': p['nome'],
                        'clima': p['clima'], 'terreno': p['terreno'],
                        'filmes': p['filmes']})
-    return jsonify({'result' : output})
+    return jsonify({'result': output})
+
 
 @app.route('/planet/<planet_id>', methods=['GET'])
 def get_one_planet_by_id(planet_id):
     planet = mongo.db.planets
     try:
-        p = planet.find_one_or_404({'_id' : ObjectId(planet_id)})
+        p = planet.find_one_or_404({'_id': ObjectId(planet_id)})
     except InvalidId as e:
         code = 400
         msg = '_id passado [{}] inválido'.format(planet_id)
         return (msg, code)
-        
+
     output = {}
     if p:
         output = {'_id': str(p['_id']), 'nome': p['nome'],
                   'clima': p['clima'], 'terreno': p['terreno'],
                   'filmes': p['filmes']}
-    return jsonify({'result' : output})
+        return jsonify({'result': output})
+
 
 @app.route('/planet/name/<planet_name>', methods=['GET'])
 def get_one_planet_by_name(planet_name):
     planet = mongo.db.planets
-    p = planet.find_one_or_404({'nome' : planet_name})
+    p = planet.find_one_or_404({'nome': planet_name})
     output = {}
     if p:
         output = {'_id': str(p['_id']), 'nome': p['nome'],
                   'clima': p['clima'], 'terreno': p['terreno'],
                   'filmes': p['filmes']}
-    return jsonify({'result' : output})
+    return jsonify({'result': output})
+
 
 @app.route('/planet', methods=['POST'])
 def add_star():
@@ -59,26 +63,27 @@ def add_star():
         "terreno": request.json['terreno'],
         "filmes": 0
     }
-    p = planet.find_one({'nome' : planet_data['nome']})
+    p = planet.find_one({'nome': planet_data['nome']})
     if p:
         code = 400
         msg = 'O Planeta [{}] já existe'.format(planet_data['nome'])
         return (msg, code)
     else:
         planet_id = planet.insert(planet_data)
-        new_planet = planet.find_one({'_id': planet_id })
+        new_planet = planet.find_one({'_id': planet_id})
         output = {}
         for item in planet_data:
             output[item] = new_planet[item]
         output['_id'] = str(new_planet['_id'])
 
-    return jsonify({'result' : output})
+    return jsonify({'result': output})
+
 
 @app.route('/planet/<planet_id>', methods=['DELETE'])
 def delete_one_planet_by_id(planet_id):
     planet = mongo.db.planets
     try:
-        ret = planet.delete_one({'_id' : ObjectId(planet_id)})
+        ret = planet.delete_one({'_id': ObjectId(planet_id)})
         if ret.deleted_count < 1:
             raise Exception('_id não encontrado')
         msg = ''
@@ -88,6 +93,7 @@ def delete_one_planet_by_id(planet_id):
             planet_id, e)
         code = 400
     return (msg, code)
+
 
 if __name__ == '__main__':
     mongo = PyMongo(app)
