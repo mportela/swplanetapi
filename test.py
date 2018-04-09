6d0c23c64469e36d43185c7002d4270c35405cbf
mportela <marcel.portela@gmail.com>
2018-04-09 19:32:56 -0300

    + implementado test_get_all já adicionando dados para o teste + implementado tearDown dos testes para manter o cenario de cada

--- test.py
+++ test.py
@@ -6,17 +6,43 @@ from flask_pymongo import PyMongo
 
 class SWAPITestCase(unittest.TestCase):
 
+    def _clear_data(self):
+        with self.ac.app_context():
+            planet = self.mongo.db.planets
+            for p in planet.find():
+                planet.delete_one({'_id': p['_id']})
+
     def setUp(self):
         app.app.config['MONGO_DBNAME'] = 'unittestdb'
         app.app.config['MONGO_URI'] = 'mongodb://mongodb:27017/unittestdb'
         app.app.testing = True
-        app.mongo = PyMongo(app.app)
+        if not hasattr(app, 'mongo'):
+            app.mongo = PyMongo(app.app)
         self.app = app.app.test_client()
+        self.ac = app.app
+        self.mongo = app.mongo
 
     def test_get_empty_db(self):
         ret = self.app.get('/planet')
         ret = json.loads(ret.data)
         self.assertEqual(ret['result'], [])
+
+    def test_get_all(self):
+        with self.ac.app_context():
+            planet = self.mongo.db.planets
+            planet.insert({'nome': 'teste1', 'terreno': 'acidentado',
+                           'clima': 'frio', 'filmes': 0})
+            planet.insert({'nome': 'teste planeta espécial',
+                           'terreno': 'liso',
+                           'clima': 'quente', 'filmes': 0})
+
+        ret = self.app.get('/planet')
+        ret = json.loads(ret.data)
+        print(ret)
+
+    def tearDown(self):
+        self._clear_data()
+
 
 if __name__ == '__main__':
     unittest.main()
